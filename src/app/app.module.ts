ee6d51911a903c135181f3d8cc8b6d744d4bc5d0
athulprakash007 <athull362@gmail.com>
2022-12-13 22:35:18 -0800

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoronaComponent } from './corona/corona.component';
+import { CoronastateComponent } from './coronastate/coronastate.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+const myroutes=[
+  {path:"",component:CoronaComponent},
+  {path:"corona",component:CoronastateComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoronaComponent,
+    CoronastateComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     RouterModule.forRoot(myroutes),
+     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
